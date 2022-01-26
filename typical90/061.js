846fb25492e1b84dbb641a32410f4f055d8917c4
hoshikagi96 <hoshikagi96@gmail.com>
2022-01-26 23:53:31 +0900

    solved typical90-061

---
+++ typical90/061.js
@@ -0,0 +1,27 @@
+'use strict'
+
+function main(input) {
+    const tmp = input.split('\n');
+    const Q = Number(tmp.shift());
+
+    const cards = [];
+    for (let i = 0; i < Q; i++) {
+        const [t, x] = tmp[i].split(' ').map(Number);
+        if (t === 1) {
+            cards.unshift(x);
+        } else if (t === 2) {
+            cards.push(x);
+        } else if (t === 3) {
+            console.log(cards[x - 1]);
+        }
+    }
+}
+
+// main(require("fs").readFileSync("/dev/stdin", "utf8"));
+main(`6
+1 2
+1 1
+2 3
+3 1
+3 2
+3 3`)
